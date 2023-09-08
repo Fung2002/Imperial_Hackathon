81a5a992951d51e1314f5f6e5b30ff27f0f45550
Mark <10621536+markliederbach@users.noreply.github.com>
2023-09-08 16:25:58 -0500

    checkpoint

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,14 @@
 <script>
 	import AudioController from './AudioController.svelte';
+	import Oscilloscope from './Oscilloscope.svelte';
 </script>
 
-<div class="controller absolute bottom-6 left-6">
-	<AudioController src="https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3" />
+<div class="flex flex-col h-screen items-stretch">
+	<div class="visualizer flex-grow">
+		<Oscilloscope />
+	</div>
+
+	<div class="controller fixed bottom-0 left-0 m-4">
+		<AudioController src="https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3" />
+	</div>
 </div>
---
+++ src/routes/Oscilloscope.svelte
@@ -0,0 +1,21 @@
+<script>
+	import { onMount } from 'svelte';
+
+	// https://davidmatthew.ie/creating-an-oscilloscope-with-javascript/
+	let audioCtx;
+
+	async function startAudioCtx() {
+		// https://www.okupter.com/blog/sveltekit-window-is-not-defined
+		const AudioContext = window.AudioContext || window.webkitAudioContext;
+		audioCtx = new AudioContext();
+	}
+
+	async function stopAudioCtx() {
+		if (audioCtx) {
+			audioCtx.close();
+		}
+		audioCtx = null;
+	}
+</script>
+
+<canvas />

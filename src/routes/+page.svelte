41dd2e02ec9ed81646215d7a51b0e072ba9e1809
Mark <10621536+markliederbach@users.noreply.github.com>
2023-09-08 14:55:32 -0500

    checkpoint

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,7 @@
-<button class="btn">Hello daisyUI</button>
+<script>
+	import AudioController from './AudioController.svelte';
+</script>
+
+<div class="centered">
+	<AudioController src="https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3" />
+</div>
---
+++ src/routes/AudioController.svelte
@@ -0,0 +1,32 @@
+<script>
+	/**
+	 * @type {any}
+	 */
+	export let src;
+
+	/**
+	 * @type {number}
+	 */
+	let time;
+	let duration;
+	let paused = true;
+</script>
+
+<div class="controller" class:paused>
+	<audio
+		{src}
+		bind:currentTime={time}
+		bind:duration
+		bind:paused
+		on:ended={() => {
+			time = 0;
+		}}
+	/>
+	<button
+		class="btn-lg btn-secondary btn-circle"
+		aria-label={paused ? 'play' : 'pause'}
+		on:click={() => {
+			paused = !paused;
+		}}
+	/>
+</div>

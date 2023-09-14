17b2e897512fe389de936807599b53ba36317f3e
Mark <10621536+markliederbach@users.noreply.github.com>
2023-09-14 12:33:08 -0500

    docker

--- src/routes/Oscilloscope.svelte
+++ src/routes/Oscilloscope.svelte
@@ -4,29 +4,46 @@
 	import { Microphone } from '$lib/microphone';
 	import { Bar } from '$lib/bars';
 
-	const recordingStrokeStyle = '#2dd4bf';
-	const pausedStrokeStyle = '#374151';
+	// Constants
+	const RECORDING_STROKE_STYLE = '#2dd4bf';
+	const PAUSED_STROKE_STYLE = '#374151';
+
+	// Instance Variables
 	const microphone = new Microphone();
 	const numOfBars = microphone.fftSize / 2;
-
 	let bars = [];
 	let canvas;
 	let canvasCtx;
 
+	// Reactive statement to draw canvas when recording state changes
 	$: $recording, draw();
 
+	// On component mount
+	onMount(() => {
+		canvasCtx = canvas.getContext('2d');
+		createBars();
+		handleWindowResize();
+
+		window.addEventListener('resize', handleWindowResize);
+
+		return () => {
+			window.removeEventListener('resize', handleWindowResize);
+		};
+	});
+
+	// Functions
 	function draw() {
-		// Set canvas styling
 		if (!canvasCtx) return;
 
 		drawFlatLine();
 
-		// Stop listening if user pauses
+		// Toggle microphone listening state based on recording state
 		if (!$recording) {
 			microphone.disconnect();
 		} else {
 			microphone.listen();
 		}
+
 		animate();
 	}
 
@@ -34,13 +51,13 @@
 		if (!canvasCtx) return;
 
 		canvasCtx.clearRect(0, 0, canvas.width, canvas.height);
+
 		if (microphone.initialized) {
-			let samples;
-			if ($waveDomain === 'time') {
-				samples = microphone.getTimeDomainSamples(1000);
-			} else {
-				samples = microphone.getFrequencyDomainSamples(5);
-			}
+			let samples =
+				$waveDomain === 'time'
+					? microphone.getTimeDomainSamples(1000)
+					: microphone.getFrequencyDomainSamples(5);
+
 			bars.forEach((bar, i) => {
 				bar.update(samples[i]);
 				bar.draw(canvasCtx);
@@ -48,10 +65,11 @@
 		} else {
 			drawFlatLine();
 		}
+
 		requestAnimationFrame(animate);
 	}
 
-	function drawFlatLine(color) {
+	function drawFlatLine(color = PAUSED_STROKE_STYLE) {
 		canvasCtx.strokeStyle = color;
 		canvasCtx.beginPath();
 		canvasCtx.moveTo(0, canvas.height / 2);
@@ -62,33 +80,26 @@
 	function createBars() {
 		bars = [];
 		const barWidth = canvas.width / numOfBars;
+
 		for (let i = 0; i < numOfBars; i++) {
-			bars.push(new Bar(i * barWidth, canvas.height / 2, barWidth, 20, recordingStrokeStyle));
+			bars.push(new Bar(i * barWidth, canvas.height / 2, barWidth, 20, RECORDING_STROKE_STYLE));
 		}
 	}
 
-	onMount(() => {
-		canvasCtx = canvas.getContext('2d');
-
-		function resize() {
-			canvas.width = window.innerWidth;
-			canvas.height = window.innerHeight;
-			createBars();
-
-			canvasCtx.clearRect(0, 0, canvas.width, canvas.height);
-			drawFlatLine(pausedStrokeStyle);
-		}
-
+	function handleWindowResize() {
+		canvas.width = window.innerWidth;
+		canvas.height = window.innerHeight;
 		createBars();
-
-		window.addEventListener('resize', resize);
-		resize();
-
-		return () => {
-			window.removeEventListener('resize', resize);
-		};
-	});
+		drawFlatLine();
+	}
 </script>
 
-<p>{$recording ? `Listening` : 'Paused'}</p>
-<canvas bind:this={canvas} class="w-full h-full" />
+<p>{$recording ? 'Listening' : 'Paused'}</p>
+<canvas bind:this={canvas} class="full-size" />
+
+<style>
+	.full-size {
+		width: 100%;
+		height: 100%;
+	}
+</style>
